a7158662252371df5cfb299a99bdc43961f3aa55
Yanuo Ma <myn508@hotmail.com>
2024-10-18 07:19:26 +0000

    Review and use default vscode-eslint settings

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ local M = {}
 
 function M.use_flat_config(root_dir)
   if
-    vim.fn.filereadable(root_dir .. '/eslint.config.js') == 1
-    or vim.fn.filereadable(root_dir .. '/eslint.config.mjs') == 1
-    or vim.fn.filereadable(root_dir .. '/eslint.config.cjs') == 1
-    or vim.fn.filereadable(root_dir .. '/eslint.config.ts') == 1
-    or vim.fn.filereadable(root_dir .. '/eslint.config.mts') == 1
-    or vim.fn.filereadable(root_dir .. '/eslint.config.cts') == 1
+      vim.fn.filereadable(root_dir .. '/eslint.config.js') == 1
+      or vim.fn.filereadable(root_dir .. '/eslint.config.mjs') == 1
+      or vim.fn.filereadable(root_dir .. '/eslint.config.cjs') == 1
+      or vim.fn.filereadable(root_dir .. '/eslint.config.ts') == 1
+      or vim.fn.filereadable(root_dir .. '/eslint.config.mts') == 1
+      or vim.fn.filereadable(root_dir .. '/eslint.config.cts') == 1
   then
     return true
   end
@@ -36,43 +36,32 @@ function M.resolve_node_path()
   return result
 end
 
+function M.create_cmd()
+  local debug = false -- for debugging language server
+  if debug then
+    return { 'node', '--inspect-brk', vim.fn.stdpath('config') ..
+    '/lua/nvim-eslint/vscode-eslint/server/out/eslintServer.js', '--stdio' }
+  end
+  return { 'node', vim.fn.stdpath('config') .. '/lua/nvim-eslint/vscode-eslint/server/out/eslintServer.js', '--stdio' }
+end
+
 function M.setup_autocmd()
   vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'typescript', 'typescriptreact' },
     callback = function(args)
       local root_dir = vim.fs.root(args.buf, { 'package.json' });
+
       vim.lsp.start({
         name = 'eslint',
-        cmd = { 'node', '--inspect-brk',vim.fn.stdpath('config') .. '/lua/nvim-eslint/vscode-eslint/server/out/eslintServer.js', '--stdio' },
+        cmd = M.create_cmd(),
         root_dir = root_dir,
         settings = {
           validate = 'on',
-          packageManager = 'pnpm',
-          useESLintClass = false,
+          -- packageManager = 'pnpm',
+          useESLintClass = true,
+          useFlatConfig = M.use_flat_config(root_dir),
           experimental = {
             useFlatConfig = false,
-          },
-          useFlatConfig = M.use_flat_config(root_dir),
-          codeActionOnSave = {
-            enable = false,
-            mode = 'all',
-          },
-          format = true,
-          quiet = false,
-          onIgnoredFiles = 'off',
-          rulesCustomizations = {},
-          run = 'onType',
-          problems = {
-            shortenToSingleLine = false,
-          },
-          -- nodePath configures the directory in which the eslint server should start its node_modules resolution.
-            -- This path is relative to the workspace folder (root dir) of the server instance.
-          nodePath = M.resolve_node_path(),
-          -- use the workspace folder location or the file location (if no workspace folder is open) as the working directory
-          workingDirectory = { directory = root_dir },
-          workspaceFolder = {
-            uri = root_dir,
-            name = vim.fn.fnamemodify(root_dir, ':t'),
           },
           codeAction = {
             disableRuleComment = {
@@ -83,6 +72,28 @@ function M.setup_autocmd()
               enable = true,
             },
           },
+          codeActionOnSave = {
+            mode = 'all',
+          },
+          format = false,
+          quiet = false,
+          onIgnoredFiles = 'off',
+          options = {},
+          rulesCustomizations = {},
+          run = 'onType',
+          problems = {
+            shortenToSingleLine = false,
+          },
+          -- nodePath configures the directory in which the eslint server should start its node_modules resolution.
+          -- This path is relative to the workspace folder (root dir) of the server instance.
+          nodePath = M.resolve_node_path(),
+          -- use the workspace folder location or the file location (if no workspace folder is open) as the working directory
+          workingDirectory = { directory = root_dir },
+          workspaceFolder = {
+            uri = root_dir,
+            name = vim.fn.fnamemodify(root_dir, ':t'),
+          },
+
         },
       })
     end
@@ -92,4 +103,5 @@ end
 function M.setup()
   M.setup_autocmd()
 end
+
 return M
